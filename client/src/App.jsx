0b614e189c0aec121f39d96e3ae0f256cae4ca1a
HarshKhaire <harshkhaire1001@gmail.com>
2024-09-28 18:32:41 +0530

    add home page with search bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./layout.scss";
 import Navbar from './components/navbar/Navbar';
+import HomePage from './routes/homePage/homePage';
 
 function App() {
   return (
     <div className='layout'>
       <Navbar />
+      <HomePage />
     </div>
   )
 }
