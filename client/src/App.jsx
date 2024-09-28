7ba8f71c7c18eb0d59297f5af7246374e8cbfee9
HarshKhaire <harshkhaire1001@gmail.com>
2024-09-28 19:10:20 +0530

    add browser routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
-import "./layout.scss";
-import Navbar from './components/navbar/Navbar';
 import HomePage from './routes/homePage/homePage';
+import ListPage from './routes/listPage/ListPage';
+import {
+  createBrowserRouter, 
+  RouterProvider
+} from "react-router-dom";
+import { Layout } from './routes/layout/Layout';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path : "/",
+          element: <HomePage />
+        },
+        {
+          path : "/list",
+          element: <ListPage />
+        }
+      ]
+    },
+  ]);
+
   return (
-    <div className='layout'>
-      <Navbar />
-      <HomePage />
-    </div>
+    <RouterProvider router = {router}/>
   )
 }
 
-export default App
+export default App;
