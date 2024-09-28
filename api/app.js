96b54fa5ea58670df30fc52a1ae5a8af9ddbaac4
HarshKhaire <harshkhaire1001@gmail.com>
2024-09-28 16:00:07 +0530

    add auth routes, connect to mongo db and create user schema

--- api/app.js
+++ api/app.js
@@ -1,11 +1,16 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import postRoute from "./routes/post.route.js";
+import authRoute from "./routes/auth.route.js";
 
 const port = 2000;
 const app = express();
 
-app.use("/api/test", (req, res) => {
-    res.send("<h2>Server working !!</h2>");
-})
+app.use(express.json());
+app.use(cookieParser( ));
+
+app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
