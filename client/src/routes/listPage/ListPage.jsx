7b7f2b72af9386310da0df29030bbb8b1f4a3f22
HarshKhaire <harshkhaire1001@gmail.com>
2024-09-29 14:43:26 +0530

    add house list page with card components

--- client/src/routes/listPage/ListPage.jsx
+++ client/src/routes/listPage/ListPage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import "./listPage.scss";
+import Filter from "../../components/filter/Filter";
+import Card from "../../components/card/Card";
+import Map from "../../components/map/Map";
+import {listData} from "../../lib/dummydata.js";
 function ListPage() {
+  const data = listData;
+
   return (
-    <div>ListPage</div>
-  )
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map(item =>(
+            <Card key = {item.id} item ={item}/>
+          ))}
+        </div>
+      </div>
+      <d iv className="mapContainer">
+        
+      </d>
+    </div>
+  );
 }
 
-export default ListPage
+export default ListPage;
