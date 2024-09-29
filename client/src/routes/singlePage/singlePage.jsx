0999b0cd85c44f3b4c90b3807e634e97719758ff
HarshKhaire <harshkhaire1001@gmail.com>
2024-09-29 17:50:53 +0530

    create single page UI

--- client/src/routes/singlePage/singlePage.jsx
+++ client/src/routes/singlePage/singlePage.jsx
@@ -1,5 +1,6 @@
 import "./singlePage.scss";
 import Slider from "../../components/slider/Slider";
+import Map from "../../components/map/Map";
 import {singlePostData, userData} from "../../lib/dummydata.js"
 
 function SinglePage() {
@@ -29,8 +30,96 @@ function SinglePage() {
           </div>
         </div>
       </div>
+
+      <div className="features">
+        <div className="wrapper">
+          <p className="title">General</p>
+          <div className="listVertical">
+            <div className="feature">
+              <img src="/utility.png" alt="" />
+              <div className="featureText">
+                <span>Utilities</span>
+                <p>Tenant is responsible</p> 
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="" />
+              <div className="featureText">
+                <span>Pet Policy</span>
+                  <p>Pets not Allowed</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/fee.png" alt="" />
+              <div className="featureText">
+                <span>Property Fees</span>
+                <p>lorem ipsum</p>
+              </div>
+            </div>
+          </div>
+          <p className="title">Sizes</p>
+          <div className="sizes">
+            <div className="size">
+              <img src="/size.png" alt="" />
+              <span>{singlePostData.size} sqft</span>
+            </div>
+            <div className="size">
+              <img src="/bed.png" alt="" />
+              <span>beds</span>
+            </div>
+            <div className="size">
+              <img src="/bath.png" alt="" />
+              <span>bathroom</span>
+            </div>
+          </div>
+          <p className="title">Nearby Places</p>
+          <div className="listHorizontal">
+            <div className="feature">
+              <img src="/school.png" alt="" />
+              <div className="featureText">
+                <span>School</span>
+                <p>
+                  {singlePostData.school > 999
+                    ? singlePostData.school / 1000 + "km"
+                    : singlePostData.school + "m"}{" "}
+                  away
+                </p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="" />
+              <div className="featureText">
+                <span>Bus Stop</span>
+                <p>m away</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/fee.png" alt="" />
+              <div className="featureText">
+                <span>Restaurant</span>
+                <p>m away</p>
+              </div>
+            </div>
+          </div>
+          <p className="title">Location</p>
+          <div className="mapContainer">
+            <Map items={[singlePostData]} />
+          </div>
+          <div className="buttons">
+            <button>
+              <img src="/chat.png" alt="" />
+              Send a Message
+            </button>
+            <button >
+              <img src="/save.png" alt="" />
+              Save the Place 
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default SinglePage;
+
